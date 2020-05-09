609d29cfff0d81a343a872b179cebe4412c6cbb7
МС <ms@MacBook-Pro-MS.local>
2020-05-09 13:32:31 +0300

    сделал новый дизайн для добавление записи в журнал

--- src/components/event-log/add-event/add-event.js
+++ src/components/event-log/add-event/add-event.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from './add-event.module.css';
 import TextField from '@material-ui/core/TextField';
+import {InputLabel} from '@material-ui/core';
 import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
 import  { Redirect } from 'react-router-dom';
@@ -18,21 +19,37 @@ export default class AddEvent extends React.Component{
 		    // debugger;
             e.preventDefault();
             this.props.setEventItem({name:this.state.text})
-            this.setState({text:''})
+			this.setState({text:''})
 			this.props.history.goBack();
       };
 	
       render(){
         return(
           <div className={style.add_event}>
-           <form onSubmit={this.submitText}>
-            <TextField multiline rows={4}
-					   variant="outlined"
-                       value={this.state.text}
-                       onChange={this.changeText} 
-                        />
-            <Input color='primary' type='submit' />
-           </form>
+		   {/*<form onSubmit={this.submitText}>*/}
+			<div className={style.text}>
+				<InputLabel color='primary'>Заголовок</InputLabel>
+			 	{/*<input */}
+				{/*<TextField multiline rows={1}*/}
+				<input	   placeholder='введите заголовок'
+						   variant="outlined"
+						   value={this.state.text}
+						   onChange={this.changeText} 
+						   // className={style.input}
+							/>
+			<InputLabel color='primary'>Описание</InputLabel>
+			<textarea placeholder='введите описание' />
+			{/*<TextField multiline rows={10}*/}
+					   {/*variant='outlined'*/}
+					   {/*className={style.input}		   */}
+						{/*/>*/}
+				</div>
+			<div>
+			<Button color='primary' variant='outlined'>Сохранить</Button>
+			<Button color='primary' variant='outlined'>Отменить</Button>
+			</div>
+            {/*<Input color='primary' type='submit' />*/}
+           {/*</form>*/}
           </div>
           )
         }
